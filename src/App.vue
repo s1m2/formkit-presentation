b6c61265a317db096cfdccd505591261f18893d3
Sam Macharia <smm_mwangii@live.com>
2023-09-15 11:25:21 +0100

    formkit schema

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,52 @@
-<script setup>
-async function submit() {
-  await new Promise(r => setTimeout(r, 1000))
-  alert('Submitted! 🎉')
+<script setup lang="ts">
+import { FormKitSchema } from '@formkit/vue';
+
+const schema = [
+  {
+    $formkit: 'text',
+    name: 'email',
+    label: 'Email',
+    help: 'This will be used for your account.',
+    validation: 'required|email'
+  },
+  {
+    $formkit: 'password',
+    name: 'password',
+    label: 'Password',
+    help: 'Enter your new password.',
+    validation: 'required|length:5,16'
+  },
+  {
+    $formkit: 'password',
+    name: 'password_confirm',
+    label: 'Confirm password',
+    help: 'Enter your new password again to confirm it.',
+    validation: 'required|confirm',
+    validationLabel: 'password confirmation'
+  },
+  {
+    $formkit: 'checkbox',
+    name: 'eu_citizen',
+    id: 'eu',
+    label: 'Are you a european citizen?',
+    validation: 'required'
+  },
+  {
+    $formkit: 'select',
+    if: '$get(eu).value',
+    name: 'cookie_notice',
+    label: 'Cookie notice frequency',
+    options: {
+      refresh: 'Every page load',
+      hourly: 'Ever hour',
+      daily: 'Every day',
+    },
+    help: 'How often should we display a cookie notice?'
+  },
+]
+
+function register(data: Record<string, any>) {
+  alert(data);
 }
 </script>
 
@@ -15,7 +60,9 @@ async function submit() {
       class="logo"
     >
 
-    <FormKit type="text" name="Name" label="Name" help="Your name goes here" />
+    <FormKit type="form" @submit="register">
+      <FormKitSchema :schema="schema" />
+    </FormKit>
   </div>
 </template>
 
