7d4d1bcb3dd032adef3698ba921ce2151c81aaa1
Sam Macharia <smm_mwangii@live.com>
2023-09-15 09:35:11 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+async function submit() {
+  await new Promise(r => setTimeout(r, 1000))
+  alert('Submitted! 🎉')
+}
+</script>
+
+<template>
+  <div class="your-first-form">
+    <img
+      src="https://pro.formkit.com/logo.svg"
+      alt="FormKit Logo"
+      width="244"
+      height="50"
+      class="logo"
+    >
+
+    <FormKit type="text" name="Name" label="Name" help="Your name goes here" />
+  </div>
+</template>
+
+<style scoped>
+.your-first-form {
+  width: calc(100% - 2em);
+  max-width: 480px;
+  box-sizing: border-box;
+  padding: 2em;
+  box-shadow: 0 0 1em rgba(0, 0, 0, .1);
+  border-radius: .5em;
+  margin: 4em auto;
+}
+
+.logo {
+  width: 150px;
+  height: auto;
+  display: block;
+  margin: 0 auto 2em auto;
+}
+pre {
+  background-color: rgba(0, 100, 250, .1);
+  padding: 1em;
+}
+</style>
